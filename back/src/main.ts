b0213570ae95eb51a4fdb519095f2c806a2af761
신지윤 <ranji12@naver.com>
2024-10-11 17:43:59 +0900

    cors client and server connect

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키를 허용하려면 true로 설정
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3001);
 }
 bootstrap();
