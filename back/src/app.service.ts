8b7eea458545e739cb89c93c82910df2ec648241
신지윤 <ranji12@naver.com>
2024-10-12 23:56:29 +0900

    server service 사용자 답변 카운팅

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UserDTO } from './dto/dto';
 
 @Injectable()
 export class AppService {
+  private likeCount = 0;
+  private dislikeCount = 0;
+  private pushoverCount = 0;
+  
   getHello(): string {
     return '커밋테스트';
+  }
+
+  sendName(user: UserDTO) {
+    console.log(user);
+    return user;
+  }
+  likeCounter(){
+    this.likeCount++;
+    console.log('likeCount:', this.likeCount); // 로그로 값 확인
+    return { like: this.likeCount };
+  }
+  dislikeCounter(){
+    this.dislikeCount++;
+    console.log('dislikeCount:', this.dislikeCount); // 로그로 값 확인
+    return { dislike: this.dislikeCount };
+  }
+  pushoverCounter(){
+    this.pushoverCount++;
+    console.log('pushoverCount:', this.pushoverCount); // 로그로 값 확인
+    return { pushover: this.pushoverCount };
   }
 }
