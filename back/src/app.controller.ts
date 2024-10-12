909ca7b431b50ae4a572cb06a946b902a4824d3b
신지윤 <ranji12@naver.com>
2024-10-12 23:55:54 +0900

    server controller connect service

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserDTO } from './dto/dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/nameSend')
+  sendName(@Body() userName: UserDTO) {
+    return this.appService.sendName(userName);
+  }
+
+  @Post('/like')
+  likeCount(){
+    return this.appService.likeCounter();
+  }
+  @Post('/dislike')
+  dislikeCount(){
+    return this.appService.dislikeCounter();
+  }
+  @Post('/pushover')
+  pushoverCount(){
+    return this.appService.pushoverCounter();
   }
 }
