2826410b3f32050fcb999b002651bd985badc273
신지윤 <ranji12@naver.com>
2024-10-22 17:38:33 +0900

    server 결과 값 객체로 변환

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -14,33 +14,31 @@ export class AppService {
   }
 
   result() {
+    const pushoverMessages = {
+      0: '전생에 칼이셨나봐요. 정말 칼 같이 거절을 잘 하시네요.',
+      1: '거절을 정말 잘하시는 편이세요~!',
+      2: {
+        positive: '당신은 호감상인 거절을 잘해내는 분이네요.',
+        negative: '당신은 비호감상인 거절을 잘해내는 분이네요.',
+      },
+      3: '거절 연습을 할 정도까지는 아니지만 거절을 잘 못하는 편이신 것 같습니다.',
+      4: '당신은 거절하는 연습이 필요한 것 같아요~. 연습이 필요하다면 시작을 누르시고, 필요 없으시다면 이대로 종료해도 됩니다.',
+    };
     if (this.gomin !== 0) {
       return '거절을 할 때가 있고 안 할 때가 있답니다..';
-    } else if (this.likeCount > this.dislikeCount) {
-      if (this.pushoverCount === 0) {
-        return '전생에 칼이셨나봐요. 정말 칼 같이 거절을 잘 하시네요.';
-      } else if (this.pushoverCount === 1) {
-        return '거절을 정말 잘하시는 편이세요~!';
-      } else if (this.pushoverCount === 2) {
-        return '당신은 호감상인 거절을 잘해내는 분이네요.';
-      } else if (this.pushoverCount === 3) {
-        return '거절 연습을 할 정도까지는 아니지만 거절을 잘 못하는 편이신 것 같습니다.';
-      } else if (this.pushoverCount === 4) {
-        return '당신은 거절하는 연습이 필요한 것 같아요~. 연습이 필요하다면 시작을 누르시고, 필요 없으시다면 이대로 종료해도됩니다.';
-      }
-    } else {
-      if (this.pushoverCount === 0) {
-        return '전생에 칼이셨나봐요. 정말 칼 같이 거절을 잘 하시네요.';
-      } else if (this.pushoverCount === 1) {
-        return '거절을 정말 잘하시는 편이세요~! 근데 살짝 호감형인 사람이 되어볼까요?';
-      } else if (this.pushoverCount === 2) {
-        return '당신은 비호감상인 거절을 잘해내는 분이네요.';
-      } else if (this.pushoverCount === 3) {
-        return '거절 연습을 할 정도까지는 아니지만 거절을 잘 못하는 편이신 것 같습니다. 비호감적인 면이 보이는 것 같습니다. 유의하셔야할 것 같아요!';
-      } else if (this.pushoverCount === 4) {
-        return '당신은 거절하는 연습이 필요한 것 같아요~ 연습이 필요하다면 시작을 누르시고, 필요 없으시다면 이대로 종료해도됩니다.';
-      }
     }
+    const pushoverMessage =
+      this.pushoverCount === 2
+        ? this.likeCount > this.dislikeCount
+          ? pushoverMessages[2].positive
+          : pushoverMessages[2].negative
+        : pushoverMessages[this.pushoverCount];
+    const additionalMessage =
+      this.likeCount > this.dislikeCount
+        ? ''
+        : ' 근데 살짝 호감형인 사람이 되어볼까요?';
+
+    return pushoverMessage + additionalMessage;
   }
 
   sendName(user: UserDTO) {
