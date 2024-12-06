94b9bc995cfd77b00762565fb761ece6dac23efb
신지윤 <ranji12@naver.com>
2024-12-06 19:04:51 +0900

    메인 화면 css 완성

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -15,20 +15,20 @@ export default function Home() {
   }
   return (
     <div className="w-full h-full">
-      <div className="flex flex-col items-center w-100 h-1/6 bg-slate-200 justify-center">  
-        <div className="text-7xl font-black text-cyan-950 w-[1000px] h-28 flex items-center justify-center border-4 border-solid border-e-blue-500">
+      <div className="flex flex-col items-center w-100 h-1/6 justify-center">  
+        <div className="text-7xl font-black text-cyan-950 w-[1000px] h-28 flex items-center justify-center border-4 border-double border-opacity-30 shadow-xl border-blue-400 bg-slate-200">
           <div className="drop-shadow-[8px_12px_4px_rgba(0,0,0,0.8)]">
             거절을 못하는 당신을 위하여..
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-10 items-center justify-center h-4/6 bg-black mt-10">
-        <div className="bg-blue-50 w-32 h-10 flex justify-center items-center rounded-xl shadow-lg shadow-blue-500 font-bold">
+      <div className="flex flex-col gap-10 items-center justify-center h-4/6 bg-neutral-100 mt-10">
+        <div className="bg-blue-50 w-32 h-10 flex justify-center items-center rounded-xl shadow-lg shadow-blue-500 font-bold hover:bg-blue-100">
           <div onClick={explanModalClick} className="text-black">
             게임 설명
           </div>
         </div>
-        <div className="bg-red-50 w-32 h-10 flex justify-center items-center shadow-lg shadow-red-500 rounded-xl font-bold">
+        <div className="bg-red-50 w-32 h-10 flex justify-center items-center shadow-lg shadow-red-500 rounded-xl font-bold hover:bg-red-100">
           <div onClick={modalClick} className="text-black">
             게임 시작
           </div>
