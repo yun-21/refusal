e42a16700032ea7a1d6065c698cfbc07cc7b1911
신지윤 <ranji12@naver.com>
2024-12-07 23:58:19 +0900

    탭창, 버튼 커스텀 중

--- client/src/app/name_question/page.tsx
+++ client/src/app/name_question/page.tsx
@@ -27,14 +27,14 @@ export default function Name() {
   }
   return (
     <div className='text-black w-full h-full flex flex-col justify-center items-center'>
-      <div className='bg-gray-400 w-96 h-6'>
-        x
+      <div className='bg-blue-700 w-96 h-6'>
+        Plz answer me
       </div>
       <div className='bg-red-300 w-96 h-52 '>
         <h1>당신의 이름은 무엇입니까?</h1>
         <div>저장되는 것은 아니오니, 안심하세요.</div>
         <input type="text" onChange={nameChange} placeholder="이름 적으시오." />
-        <div onClick={nameSend}>시작하기</div>
+        <div onClick={nameSend} className='bg-gray-500 w-20 text-center shadow-md'>시작하기</div>
       </div>
     </div>
   );
