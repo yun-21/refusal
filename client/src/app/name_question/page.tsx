b8afe0ebb4a014c699af2e87dc3b47957a0815b6
신지윤 <ranji12@naver.com>
2024-12-07 23:54:10 +0900

    이름 받는 페이지 옛날 윈도우 창느낌으로 커스텀 시작

--- client/src/app/name_question/page.tsx
+++ client/src/app/name_question/page.tsx
@@ -12,25 +12,30 @@ export default function Name() {
       },
       body: JSON.stringify({ name })
     })
-    .then((res)=>{
-      console.log(res);
-      // 브라우저 환경에서만 sessionStorage 사용
-      if (typeof window !== 'undefined') {
-        sessionStorage.setItem('name', name);
-      }
-      router.push('/first_question');
-    })
-    .catch((error) => console.error('Error:', error));
+      .then((res) => {
+        console.log(res);
+        // 브라우저 환경에서만 sessionStorage 사용
+        if (typeof window !== 'undefined') {
+          sessionStorage.setItem('name', name);
+        }
+        router.push('/first_question');
+      })
+      .catch((error) => console.error('Error:', error));
   }
   const nameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   }
   return (
-    <div>
-      <h1>당신의 이름은 무엇입니까?</h1>
-      <div>저장되는 것은 아니오니, 안심하세요.</div>
-      <input type="text" onChange={nameChange} placeholder="이름 적으시오." />
-      <div onClick={nameSend}>시작하기</div>
+    <div className='text-black w-full h-full flex flex-col justify-center items-center'>
+      <div className='bg-gray-400 w-96 h-6'>
+        x
+      </div>
+      <div className='bg-red-300 w-96 h-52 '>
+        <h1>당신의 이름은 무엇입니까?</h1>
+        <div>저장되는 것은 아니오니, 안심하세요.</div>
+        <input type="text" onChange={nameChange} placeholder="이름 적으시오." />
+        <div onClick={nameSend}>시작하기</div>
+      </div>
     </div>
   );
 }
