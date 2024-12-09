5ebe6c60fbee77740c2c79b5d9af9890f0805892
신지윤 <ranji12@naver.com>
2024-12-09 23:57:04 +0900

    답변창 정렬

--- client/src/app/name_question/page.tsx
+++ client/src/app/name_question/page.tsx
@@ -31,8 +31,8 @@ export default function Name() {
         <div>Plz answer me</div>
         <div className='ml-64 bg-zinc-200 w-4 h-4 flex justify-center items-center mt-1'>x</div>
       </div>
-      <div className='bg-red-300 w-96 h-52'>
-        <h1>당신의 이름은 무엇입니까?</h1>
+      <div className='bg-red-300 w-96 h-52 flex flex-col items-center justify-center gap-1'>
+        <div>당신의 이름은 무엇입니까?</div>
         <div>저장되는 것은 아니오니, 안심하세요.</div>
         <input type="text" onChange={nameChange} placeholder="이름 적으시오." />
         <div onClick={nameSend} className='bg-gray-400 w-20 text-center border-gray-500 border-x-2 border-b-2 border-r-4 border-t-gray-300 border-t-2 shadow-md mt-2'>시작하기</div>
